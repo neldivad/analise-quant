8365b8abd3449dd124206615af6e70db29742fbb
Roberto Martins <rraires@gmail.com>
2021-07-06 10:58:27 -0300

    Adicionado Calculo de Rentabilidade

--- quant_app_carteira.py
+++ quant_app_carteira.py
@@ -117,8 +117,8 @@ def botao_inserir():
         setor = fundamentus.get_papel(st.session_state.papel)['Setor'][0]
         subsetor = fundamentus.get_papel(st.session_state.papel)['Subsetor'][0]
       except:
-        setor = 'ETFs / FIIs'
-        subsetor = 'ETFs / FIIs'
+        setor = 'ETFs/FIIs/BDRs'
+        subsetor = 'ETFs/FIIs/BDRs'
       st.session_state.portifolio = st.session_state.portifolio.append({'Ação': st.session_state.papel, 'Qtde': st.session_state.lote, 'Últ. Preço': ultimo_preco, 'Valor na Carteira': valor_total,
                                                   'Setor': setor, 'SubSetor': subsetor, 'Beta do Ativo': beta}, ignore_index=True)
       calc_porc_e_betapond()
