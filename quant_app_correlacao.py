aeb5a720980ce3dde43f10b240ec0e28ebf51aa6
Roberto Martins <rraires@gmail.com>
2021-07-12 17:18:36 -0300

    Adicionado pagina de contato

--- quant_app_correlacao.py
+++ quant_app_correlacao.py
@@ -31,6 +31,7 @@ def calcular_correlacoes():
       tickers = [item + '.SA' for item in st.session_state.tickers_sel] # Adicionar o '.SA' nos tickers
       tickers += ['^BVSP','^GSPC', 'USDBRL=X']
       #st.session_state.tickers_corr = st.session_state.tickers_sel + ['^BVSP', 'USDBRL=X'] # Adiciona os Indices para comparação
+
       col1, col2 = st.beta_columns(2)
       with col1:
         periodo = st.selectbox('Período:',['1 ano', '6 meses', '3 meses'])
@@ -50,29 +51,7 @@ def calcular_correlacoes():
       correlacao = correlacao.drop('SP500',1)
       correlacao = correlacao.drop('SP500',0)
 
-      col1, col2, col3 = st.beta_columns([1,0.1,1])
-
-      with col1:
-        st.write('**Correlação entre os Ativos**')
-        correlacao['Ação 1'] = correlacao.index
-        correlacao = correlacao.melt(id_vars='Ação 1', var_name="Ação 2", value_name='Correlação').reset_index(drop=True)
-        correlacao = correlacao[correlacao['Ação 1'] < correlacao['Ação 2']].dropna()
-        highest_corr = correlacao.sort_values("Correlação", ascending=False)
-        highest_corr.reset_index(drop=True, inplace=True)  # Reseta o indice
-        highest_corr.index += 1  # Iniciar o index em 1 ao invés de 0
-
-        def _color_red_or_green(val):  # Função para o mapa de cores da tabela
-          color = 'red' if val < 0 else 'green'
-          # return 'color: %s' % color
-          return 'background-color: %s' % color
-
-        # highest_corr = highest_corr.style.applymap(_color_red_or_green, subset=['Correlação']).format({"Correlação": "{:.0%}"}) # Aplicar Cores
-        highest_corr = highest_corr.style.background_gradient(cmap="Oranges").format({"Correlação": "{:.0%}"})
-
-        st.table(highest_corr)
-        # st.dataframe(highest_corr, height=600)
-
-      with col3:
+      if len(st.session_state.tickers_sel)== 1: # Se tiver apenas 1 Ativo, mostrar apenas a correlação entre ele e os Indices
         st.write('**Correlação dos Ativos com IBOV, SP500 e Dolar**')
         corr_table_indices = pd.DataFrame(correlacao_full['IBOV'])
         corr_table_indices['SP500'] = correlacao_full['SP500']
@@ -80,23 +59,59 @@ def calcular_correlacoes():
         corr_table_indices = corr_table_indices.drop('IBOV',0)
         corr_table_indices = corr_table_indices.drop('SP500', 0)
         corr_table_indices = corr_table_indices.drop('Dolar',0)
+        corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
+        st.table(corr_table_indices)
+        #st.dataframe(corr_table_indices,width=800,height=400)
 
-        ordenar = st.selectbox('Ordenar por', ['IBOV','SP500', 'Dolar'])
-        if ordenar == 'IBOV':
-          corr_table_indices = corr_table_indices.sort_values("IBOV",ascending = False)
-          corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
-          st.table(corr_table_indices)
-          #st.dataframe(corr_table_indices,width=800,height=400)
-        if ordenar == 'SP500':
-          corr_table_indices = corr_table_indices.sort_values("SP500",ascending = False)
-          corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
-          st.table(corr_table_indices)
-          #st.dataframe(corr_table_indices, height=400)
-        if ordenar == 'Dolar':
-          corr_table_indices = corr_table_indices.sort_values("Dolar",ascending = False)
-          corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
-          st.table(corr_table_indices)
-          #st.dataframe(corr_table_indices, height=400)
+
+      else: #Se tiver mais de 1 ativo, mostrar a correlação entre eles e entre os indices
+        col1, col2, col3 = st.beta_columns([1,0.1,1])
+
+        with col1:
+          st.write('**Correlação entre os Ativos**')
+          correlacao['Ação 1'] = correlacao.index
+          correlacao = correlacao.melt(id_vars='Ação 1', var_name="Ação 2", value_name='Correlação').reset_index(drop=True)
+          correlacao = correlacao[correlacao['Ação 1'] < correlacao['Ação 2']].dropna()
+          highest_corr = correlacao.sort_values("Correlação", ascending=False)
+          highest_corr.reset_index(drop=True, inplace=True)  # Reseta o indice
+          highest_corr.index += 1  # Iniciar o index em 1 ao invés de 0
+
+          def _color_red_or_green(val):  # Função para o mapa de cores da tabela
+            color = 'red' if val < 0 else 'green'
+            # return 'color: %s' % color
+            return 'background-color: %s' % color
+
+          # highest_corr = highest_corr.style.applymap(_color_red_or_green, subset=['Correlação']).format({"Correlação": "{:.0%}"}) # Aplicar Cores
+          highest_corr = highest_corr.style.background_gradient(cmap="Oranges").format({"Correlação": "{:.0%}"})
+
+          st.table(highest_corr)
+          # st.dataframe(highest_corr, height=600)
+
+        with col3:
+          st.write('**Correlação dos Ativos com IBOV, SP500 e Dolar**')
+          corr_table_indices = pd.DataFrame(correlacao_full['IBOV'])
+          corr_table_indices['SP500'] = correlacao_full['SP500']
+          corr_table_indices['Dolar'] = correlacao_full['Dolar']
+          corr_table_indices = corr_table_indices.drop('IBOV',0)
+          corr_table_indices = corr_table_indices.drop('SP500', 0)
+          corr_table_indices = corr_table_indices.drop('Dolar',0)
+
+          ordenar = st.selectbox('Ordenar por', ['IBOV','SP500', 'Dolar'])
+          if ordenar == 'IBOV':
+            corr_table_indices = corr_table_indices.sort_values("IBOV",ascending = False)
+            corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
+            st.table(corr_table_indices)
+            #st.dataframe(corr_table_indices,width=800,height=400)
+          if ordenar == 'SP500':
+            corr_table_indices = corr_table_indices.sort_values("SP500",ascending = False)
+            corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
+            st.table(corr_table_indices)
+            #st.dataframe(corr_table_indices, height=400)
+          if ordenar == 'Dolar':
+            corr_table_indices = corr_table_indices.sort_values("Dolar",ascending = False)
+            corr_table_indices = corr_table_indices.style.background_gradient(cmap="Oranges").format({"IBOV": "{:.0%}","SP500": "{:.0%}", "Dolar": "{:.0%}"})
+            st.table(corr_table_indices)
+            #st.dataframe(corr_table_indices, height=400)
 
   with st.beta_expander("Gráfico de Correlação no Tempo", expanded=True):
     if st.checkbox('Correlação no Tempo', help='Mostrar a correlação no tempo em relação aos Indices'):
