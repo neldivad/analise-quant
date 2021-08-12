d28cedc604a34838e9ad4de45779666e9380d678
Roberto Martins <rraires@gmail.com>
2021-08-12 18:26:31 -0300

    Adicionado pagina de Raio-X do Mercado

--- quant_app_raiox.py
+++ quant_app_raiox.py
@@ -14,6 +14,7 @@ import math
 
 def raiox():
   st.header('Raio-X do Mercado')
+  st.markdown('***')
   df_lista_bolsas = pd.read_csv('./Indices_Bolsas_Mundo.csv')
   count = 0
   df_lista_bolsas['%'] = ''
@@ -22,12 +23,13 @@ def raiox():
       change = (((price.iloc[-1] - price.iloc[-2]) / price.iloc[-2]) * 100).round(2)
       df_lista_bolsas['%'][count] = change
       count += 1
+
+  st.subheader('**Comportamento das Bolsas Mundiais**')
   fig = px.choropleth(df_lista_bolsas, locations="Sigla", hover_data=['País', 'Indice', '%'])
-  fig.update_layout(showlegend=False, title_text='Comportamento das Bolsas Mundiais',
-                    margin={"r": 0, "t": 30, "l": 0, "b": 0})
+  fig.update_layout(showlegend=False,
+                    margin={"r": 0, "t": 0, "l": 0, "b": 0})
   fig.update_traces(colorscale=[(0.00, "red"), (0.50, "red"), (0.50, "green"), (1.00, "green")], zmid=0,
                     z=df_lista_bolsas['%'])
-  fig.show()
   st.plotly_chart(fig)
   st.markdown('Dados com delay de 15 min.')
   st.markdown('***Página em desenvolvimento e testes***')
