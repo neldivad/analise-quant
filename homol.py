df3e8028fc53d7c7037241fab6f294a8d41c6810
Roberto Martins <rraires@gmail.com>
2021-09-30 17:53:01 -0300

    Backtesting IFR2

--- homol.py
+++ homol.py
@@ -6,6 +6,13 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import math
 
+st.set_page_config(  # Alternate names: setup_page, page, layout
+    layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+    initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+    page_title=None,  # String or None. Strings get appended with "• Streamlit".
+    page_icon=None,  # String, anything supported by st.image, or None.
+)
+
 @st.cache
 def puxar_tickers_grafbolsa():
     url = 'http://www.grafbolsa.com/index.html'
