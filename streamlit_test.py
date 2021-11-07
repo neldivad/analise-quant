9955549e75a7156c2e555e4dac7bdfe4e3aa2103
Roberto Martins <rraires@gmail.com>
2021-11-07 09:34:15 -0300

    Atualização Tabela Tickers

--- streamlit_test.py
+++ streamlit_test.py
@@ -6,134 +6,171 @@ from itertools import cycle
 
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
+def exemplo():
+    #Example controlers
+    st.sidebar.subheader("St-AgGrid example options")
 
-#Example controlers
-st.sidebar.subheader("St-AgGrid example options")
+    sample_size = st.sidebar.number_input("rows", min_value=10, value=10)
+    grid_height = st.sidebar.number_input("Grid height", min_value=200, max_value=800, value=200)
 
-sample_size = st.sidebar.number_input("rows", min_value=10, value=10)
-grid_height = st.sidebar.number_input("Grid height", min_value=200, max_value=800, value=200)
+    return_mode = st.sidebar.selectbox("Return Mode", list(DataReturnMode.__members__), index=1)
+    return_mode_value = DataReturnMode.__members__[return_mode]
 
-return_mode = st.sidebar.selectbox("Return Mode", list(DataReturnMode.__members__), index=1)
-return_mode_value = DataReturnMode.__members__[return_mode]
+    update_mode = st.sidebar.selectbox("Update Mode", list(GridUpdateMode.__members__), index=6)
+    update_mode_value = GridUpdateMode.__members__[update_mode]
 
-update_mode = st.sidebar.selectbox("Update Mode", list(GridUpdateMode.__members__), index=6)
-update_mode_value = GridUpdateMode.__members__[update_mode]
-
-#enterprise modules
-enable_enterprise_modules = st.sidebar.checkbox("Enable Enterprise Modules")
-if enable_enterprise_modules:
-    enable_sidebar =st.sidebar.checkbox("Enable grid sidebar", value=False)
-else:
-    enable_sidebar = False
-
-#features
-fit_columns_on_grid_load = st.sidebar.checkbox("Fit Grid Columns on Load")
-
-enable_selection=st.sidebar.checkbox("Enable row selection", value=True)
-if enable_selection:
-    st.sidebar.subheader("Selection options")
-    selection_mode = st.sidebar.radio("Selection Mode", ['single','multiple'])
-    
-    use_checkbox = st.sidebar.checkbox("Use check box for selection")
-    if use_checkbox:
-        groupSelectsChildren = st.sidebar.checkbox("Group checkbox select children", value=True)
-        groupSelectsFiltered = st.sidebar.checkbox("Group checkbox includes filtered", value=True)
-
-    if ((selection_mode == 'multiple') & (not use_checkbox)):
-        rowMultiSelectWithClick = st.sidebar.checkbox("Multiselect with click (instead of holding CTRL)", value=False)
-        if not rowMultiSelectWithClick:
-            suppressRowDeselection = st.sidebar.checkbox("Suppress deselection (while holding CTRL)", value=False)
-        else:
-            suppressRowDeselection=False
-    st.sidebar.text("___")
-
-enable_pagination = st.sidebar.checkbox("Enable pagination", value=False)
-if enable_pagination:
-    st.sidebar.subheader("Pagination options")
-    paginationAutoSize = st.sidebar.checkbox("Auto pagination size", value=True)
-    if not paginationAutoSize:
-        paginationPageSize = st.sidebar.number_input("Page size", value=5, min_value=0, max_value=sample_size)
-    st.sidebar.text("___")
-
-
-df = pd.DataFrame({'month': [1, 4, 7, 10],'year': [2012, 2014, 2013, 2014],'sale':[55, 40, 84, 31]})
-
-
-#Infer basic colDefs from dataframe types
-gb = GridOptionsBuilder.from_dataframe(df)
-
-#customize gridOptions
-gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
-
-# gb.configure_column("date_tz_aware", type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='yyyy-MM-dd HH:mm zzz', pivot=True)
-
-# gb.configure_column("apple", type=["numericColumn","numberColumnFilter","customNumericFormat"], precision=2, aggFunc='sum')
-# gb.configure_column("banana", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='avg')
-# gb.configure_column("chocolate", type=["numericColumn", "numberColumnFilter", "customCurrencyFormat"], custom_currency_symbol="R$", aggFunc='max')
-
-#configures last row to use custom styles based on cell's value, injecting JsCode on components front end
-cellsytle_jscode = JsCode("""
-function(params) {
-    if (params.value == 'A') {
-        return {
-            'color': 'white',
-            'backgroundColor': 'darkred'
-        }
-    } else {
-        return {
-            'color': 'black',
-            'backgroundColor': 'white'
-        }
-    }
-};
-""")
-# gb.configure_column("group", cellStyle=cellsytle_jscode)
-
-if enable_sidebar:
-    gb.configure_side_bar()
-
-if enable_selection:
-    gb.configure_selection(selection_mode)
-    if use_checkbox:
-        gb.configure_selection(selection_mode, use_checkbox=True, groupSelectsChildren=groupSelectsChildren, groupSelectsFiltered=groupSelectsFiltered)
-    if ((selection_mode == 'multiple') & (not use_checkbox)):
-        gb.configure_selection(selection_mode, use_checkbox=False, rowMultiSelectWithClick=rowMultiSelectWithClick, suppressRowDeselection=suppressRowDeselection)
-
-if enable_pagination:
-    if paginationAutoSize:
-        gb.configure_pagination(paginationAutoPageSize=True)
+    #enterprise modules
+    enable_enterprise_modules = st.sidebar.checkbox("Enable Enterprise Modules")
+    if enable_enterprise_modules:
+        enable_sidebar =st.sidebar.checkbox("Enable grid sidebar", value=False)
     else:
-        gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=paginationPageSize)
+        enable_sidebar = False
 
-gb.configure_grid_options(domLayout='normal')
-gridOptions = gb.build()
+    #features
+    fit_columns_on_grid_load = st.sidebar.checkbox("Fit Grid Columns on Load")
 
-#Display the grid
-st.header("Streamlit Ag-Grid")
+    enable_selection=st.sidebar.checkbox("Enable row selection", value=True)
+    if enable_selection:
+        st.sidebar.subheader("Selection options")
+        selection_mode = st.sidebar.radio("Selection Mode", ['single','multiple'])
+        
+        use_checkbox = st.sidebar.checkbox("Use check box for selection")
+        if use_checkbox:
+            groupSelectsChildren = st.sidebar.checkbox("Group checkbox select children", value=True)
+            groupSelectsFiltered = st.sidebar.checkbox("Group checkbox includes filtered", value=True)
 
-grid_response = AgGrid(
-    df, 
-    gridOptions=gridOptions,
-    height=grid_height, 
-    width='100%',
-    data_return_mode=return_mode_value, 
-    update_mode=update_mode_value,
-    fit_columns_on_grid_load=fit_columns_on_grid_load,
-    allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
-    enable_enterprise_modules=enable_enterprise_modules,
-    )
+        if ((selection_mode == 'multiple') & (not use_checkbox)):
+            rowMultiSelectWithClick = st.sidebar.checkbox("Multiselect with click (instead of holding CTRL)", value=False)
+            if not rowMultiSelectWithClick:
+                suppressRowDeselection = st.sidebar.checkbox("Suppress deselection (while holding CTRL)", value=False)
+            else:
+                suppressRowDeselection=False
+        st.sidebar.text("___")
+
+    enable_pagination = st.sidebar.checkbox("Enable pagination", value=False)
+    if enable_pagination:
+        st.sidebar.subheader("Pagination options")
+        paginationAutoSize = st.sidebar.checkbox("Auto pagination size", value=True)
+        if not paginationAutoSize:
+            paginationPageSize = st.sidebar.number_input("Page size", value=5, min_value=0, max_value=sample_size)
+        st.sidebar.text("___")
+
+
+    df = pd.DataFrame({'month': [1, 4, 7, 10],'year': [2012, 2014, 2013, 2014],'sale':[55, 40, 84, 31]})
+
+
+    #Infer basic colDefs from dataframe types
+    gb = GridOptionsBuilder.from_dataframe(df)
+
+    #customize gridOptions
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+
+    # gb.configure_column("date_tz_aware", type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='yyyy-MM-dd HH:mm zzz', pivot=True)
+
+    # gb.configure_column("apple", type=["numericColumn","numberColumnFilter","customNumericFormat"], precision=2, aggFunc='sum')
+    # gb.configure_column("banana", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='avg')
+    # gb.configure_column("chocolate", type=["numericColumn", "numberColumnFilter", "customCurrencyFormat"], custom_currency_symbol="R$", aggFunc='max')
+
+    #configures last row to use custom styles based on cell's value, injecting JsCode on components front end
+
+    cellsytle_jscode = JsCode("""
+    function(params) {
+        if (params.value == 'A') {
+            return {
+                'color': 'white',
+                'backgroundColor': 'darkred'
+                
+            }
+        } else {
+            return {
+                'color': 'black',
+                'backgroundColor': 'white'
+            }
+        }
+    };
+    """)
+    # gb.configure_column("month", cellStyle=cellsytle_jscode)
+
+    if enable_sidebar:
+        gb.configure_side_bar()
+
+    if enable_selection:
+        gb.configure_selection(selection_mode)
+        if use_checkbox:
+            gb.configure_selection(selection_mode, use_checkbox=True, groupSelectsChildren=groupSelectsChildren, groupSelectsFiltered=groupSelectsFiltered)
+        if ((selection_mode == 'multiple') & (not use_checkbox)):
+            gb.configure_selection(selection_mode, use_checkbox=False, rowMultiSelectWithClick=rowMultiSelectWithClick, suppressRowDeselection=suppressRowDeselection)
+
+    if enable_pagination:
+        if paginationAutoSize:
+            gb.configure_pagination(paginationAutoPageSize=True)
+        else:
+            gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=paginationPageSize)
+
+    gb.configure_grid_options(domLayout='normal')
+    gridOptions = gb.build()
+
+    #Display the grid
+    st.header("Streamlit Ag-Grid")
+
+    grid_response = AgGrid(
+        df, 
+        gridOptions=gridOptions,
+        height=grid_height, 
+        width='100%',
+        data_return_mode=return_mode_value, 
+        update_mode=update_mode_value,
+        fit_columns_on_grid_load=fit_columns_on_grid_load,
+        allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
+        enable_enterprise_modules=enable_enterprise_modules,
+        )
 
 
 
-df = grid_response['data']
-st.dataframe(df)
-# selected = grid_response['selected_rows']
-# selected_df = pd.DataFrame(selected)
+    df = grid_response['data']
+    st.dataframe(df)
+    # selected = grid_response['selected_rows']
+    # selected_df = pd.DataFrame(selected)
 
-with st.spinner("Displaying results..."):
-    st.subheader("Returned grid data:")
+    with st.spinner("Displaying results..."):
+        st.subheader("Returned grid data:")
+        # st.dataframe(grid_response['data'])
+
+        st.subheader("grid selection:")
+        st.write(grid_response['selected_rows'])
+
+
+def homol():
+    df = pd.DataFrame({'month': [1, 4, 7, 10],'year': [2012, 2014, 2013, 2014],'sale':[55, 40, 84, 31]})
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+    gb.configure_selection('single', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
+    gb.configure_grid_options(domLayout='normal')
+    gridOptions = gb.build()
+    #Display the grid
+    st.header("Streamlit Ag-Grid")
+
+    return_mode = ['AS_INPUT', 'FILTERED', 'FILTERED_AND_SORTED']
+    return_mode_value = DataReturnMode.__members__[return_mode[2]]
+    update_mode = ['NO_UPDATE', 'MANUAL', 'VALUE_CHANGED', 'SELECTION_CHANGED', 'FILTERING_CHANGED', 'SORTING_CHANGED', 'MODEL_CHANGED']
+    update_mode_value = GridUpdateMode.__members__[update_mode[6]]
+
+    grid_response = AgGrid(
+        df, 
+        gridOptions=gridOptions,
+        height=200, 
+        width='100%',
+        data_return_mode=return_mode_value, 
+        update_mode=update_mode_value,
+        fit_columns_on_grid_load=True,
+        allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
+        )
+    # df = grid_response['data']
+    # st.dataframe(df)
+    # st.subheader("Returned grid data:")
     # st.dataframe(grid_response['data'])
+    # st.subheader("grid selection:")
+    # st.write(grid_response['selected_rows'])
 
-    st.subheader("grid selection:")
-    st.write(grid_response['selected_rows'])
-
+# exemplo()
+homol()
