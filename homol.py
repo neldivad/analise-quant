fcea7efbd90aa01b4c7b5fcef70a0750cba81d57
Roberto Martins <rraires@gmail.com>
2022-01-27 16:32:05 -0300

    Homol fundamentus

--- homol.py
+++ homol.py
@@ -1,105 +1,47 @@
 import streamlit as st
-import pandas as pd
-import requests
-import plotly.graph_objects as go
+import pandas as pd 
+import numpy as np
+import altair as alt
+from itertools import cycle
+import fundamentus
 
-st.set_page_config(layout='wide')
-st.header('API Carteira Global')
-st.markdown('***')
-key = {'x-api-key': 'vDQAZKPcub6iasVMb4l4Y4pv4xaP1uzo7ENFD6q8'}
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
-# Puxar lista dos fundos
-url = 'https://api.carteiraglobal.com/funds/'
-res = requests.get(url, headers=key)
-lista_fundos = res.json()
-df_lista_fundos = pd.DataFrame(lista_fundos['rows'])
-df_lista_fundos.set_index('id', drop=True)
+st.set_page_config(  # Alternate names: setup_page, page, layout
+    layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+    initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+    page_title=None,  # String or None. Strings get appended with "• Streamlit".
+    page_icon=None,  # String, anything supported by st.image, or None.
+)
 
-st.subheader('Fundos')
-# Escolher fundo
-selecao = st.selectbox('Selecione o Fundo', df_lista_fundos['name'])
-id = df_lista_fundos[df_lista_fundos['name'] == selecao].iloc[0][0]
-cnpj = df_lista_fundos[df_lista_fundos['name'] == selecao].iloc[0][2]
-
-# Puxar report (cotas) do fundo escolhido
-url = 'https://api.carteiraglobal.com/funds/'+str(id)+'/reports' 
-res = requests.get(url, headers=key)
-res.json()
-if len(res.json()) != 1: # Verificar se há dados de cotas
-    df_cota_fundo = pd.DataFrame(res.json())
-    df_cota_fundo['quote_norm'] = (df_cota_fundo['quote_value']/df_cota_fundo['quote_value'][0])-1
-    df_cota_fundo['date_report'] = pd.to_datetime(df_cota_fundo['date_report'])
-    df_cota_fundo.set_index('date_report', drop=True, inplace=True)
-    ult_cota = df_cota_fundo['quote_norm'][-1]
-
-    col1, col2 = st.columns([0.4,1])
-
-    with col1:
-        st.write(' ')
-        st.subheader('Fundo: ' + selecao)
-        st.subheader('CNPJ: ' + cnpj)
-        st.write(' ')
-        st.metric('Rentabilidade desde Início', value = f'{ult_cota:.0%}')
-    with col2:
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df_cota_fundo.index, y=df_cota_fundo['quote_norm']))
-        fig.update_layout(title_text = 'Histórico Rentabilidade', yaxis_tickformat = '%', width=1000, height=500)
-        st.plotly_chart(fig)
-else:
-    st.error('Este fundo não possui dados na nossa base')
+df = fundamentus.get_resultado()
+df.reset_index(inplace=True)
+df.columns=['Papel', 'Cotação', 'P/L','P/VP', 'PSR' ,'Div Yield', 'P/Ativos', 'P/Cap Giro', 'P/EBIT', 'P/Ativ Circ Liq', 
+            'EV/EBIT','EV/EBITDA', 'Marg EBIT', 'Marg Liq', 'ROIC', 'ROE', 'Liq Corr', 'Vol $ méd (2m)', 'Patrim Liq', 'Div Br/ Patrim', 'Cres Rec (5a)']
 
 
-st.markdown('***')
-st.subheader('Código Fonte')
-st.code('''
-import streamlit as st
-import pandas as pd
-import requests
-import plotly.graph_objects as go
 
-st.set_page_config(layout='wide')
-st.header('API Carteira Global')
-st.markdown('***')
-key = {'x-api-key': 'vDQAZKPcub6iasVMb4l4Y4pv4xaP1uzo7ENFD6q8'}
+#Infer basic colDefs from dataframe types
+gb = GridOptionsBuilder.from_dataframe(df)
+#customize gridOptions
+gb.configure_selection('single')
+gb.configure_grid_options(domLayout='normal')
+gridOptions = gb.build()
 
-# Puxar lista dos fundos
-url = 'https://api.carteiraglobal.com/funds/'
-res = requests.get(url, headers=key)
-lista_fundos = res.json()
-df_lista_fundos = pd.DataFrame(lista_fundos['rows'])
-df_lista_fundos.set_index('id', drop=True)
+st.header('Informações de Fundamentos')
+grid_response = AgGrid(
+    df, 
+    gridOptions=gridOptions,
+    height= 600, 
+    width='100%',
+    data_return_mode='FILTERED', 
+    update_mode='SELECTION_CHANGED',
+    fit_columns_on_grid_load=False,
+    )
 
-st.subheader('Fundos')
-# Escolher fundo
-selecao = st.selectbox('Selecione o Fundo', df_lista_fundos['name'])
-id = df_lista_fundos[df_lista_fundos['name'] == selecao].iloc[0][0]
-cnpj = df_lista_fundos[df_lista_fundos['name'] == selecao].iloc[0][2]
+# st.write(grid_response['selected_rows'][0]['papel'])
 
-# Puxar report (cotas) do fundo escolhido
-url = 'https://api.carteiraglobal.com/funds/'+str(id)+'/reports' 
-res = requests.get(url, headers=key)
-res.json()
-if len(res.json()) != 1: # Verificar se há dados de cotas
-    df_cota_fundo = pd.DataFrame(res.json())
-    df_cota_fundo['quote_norm'] = (df_cota_fundo['quote_value']/df_cota_fundo['quote_value'][0])-1
-    df_cota_fundo['date_report'] = pd.to_datetime(df_cota_fundo['date_report'])
-    df_cota_fundo.set_index('date_report', drop=True, inplace=True)
-    ult_cota = df_cota_fundo['quote_norm'][-1]
-
-    col1, col2 = st.columns([0.4,1])
-
-    with col1:
-        st.write(' ')
-        st.subheader('Fundo: ' + selecao)
-        st.subheader('CNPJ: ' + cnpj)
-        st.write(' ')
-        st.metric('Rentabilidade desde Início', value = f'{ult_cota:.0%}')
-    with col2:
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=df_cota_fundo.index, y=df_cota_fundo['quote_norm']))
-        fig.update_layout(title_text = 'Histórico Rentabilidade', yaxis_tickformat = '%', width=1000, height=500)
-        st.plotly_chart(fig)
-else:
-    st.error('Este fundo não possui dados na nossa base')
-''')
-
+# if len(grid_response['selected_rows']) == 1:
+#     st.subheader("Seleção:")
+#     selecao = fundamentus.get_papel(grid_response['selected_rows'][0]['Papel']) 
+#     st.write(selecao)
