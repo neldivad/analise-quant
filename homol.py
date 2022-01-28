ac7632efa699e29155a6e106721a5dc12a2b3068
Roberto Martins <rraires@gmail.com>
2022-01-28 08:35:58 -0300

    Teste Tradingcomdados

--- homol.py
+++ homol.py
@@ -1,47 +1,24 @@
 import streamlit as st
-import pandas as pd 
-import numpy as np
-import altair as alt
-from itertools import cycle
-import fundamentus
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objects as go
 
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
+tickers = ['PETR4.SA', 'VALE3.SA', 'EQTL3.SA' , 'CSNA3.SA', 'EMBR3.SA']
 
-st.set_page_config(  # Alternate names: setup_page, page, layout
-    layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
-    initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
-    page_title=None,  # String or None. Strings get appended with "• Streamlit".
-    page_icon=None,  # String, anything supported by st.image, or None.
-)
+ticker = st.selectbox('Ação', tickers)
 
-df = fundamentus.get_resultado()
-df.reset_index(inplace=True)
-df.columns=['Papel', 'Cotação', 'P/L','P/VP', 'PSR' ,'Div Yield', 'P/Ativos', 'P/Cap Giro', 'P/EBIT', 'P/Ativ Circ Liq', 
-            'EV/EBIT','EV/EBITDA', 'Marg EBIT', 'Marg Liq', 'ROIC', 'ROE', 'Liq Corr', 'Vol $ méd (2m)', 'Patrim Liq', 'Div Br/ Patrim', 'Cres Rec (5a)']
+periodos = ['1d', '5d','1mo', '3mo', '1y', '5y', '10y']
 
+periodo_sel = st.selectbox('Selecione o periodo', periodos)
 
+preco = pd.DataFrame(yf.download(ticker, period= periodo_sel)['Adj Close'])
 
-#Infer basic colDefs from dataframe types
-gb = GridOptionsBuilder.from_dataframe(df)
-#customize gridOptions
-gb.configure_selection('single')
-gb.configure_grid_options(domLayout='normal')
-gridOptions = gb.build()
+preco['MM20'] = preco['Adj Close'].rolling(20).mean()
 
-st.header('Informações de Fundamentos')
-grid_response = AgGrid(
-    df, 
-    gridOptions=gridOptions,
-    height= 600, 
-    width='100%',
-    data_return_mode='FILTERED', 
-    update_mode='SELECTION_CHANGED',
-    fit_columns_on_grid_load=False,
-    )
+# st.line_chart(preco)
 
-# st.write(grid_response['selected_rows'][0]['papel'])
-
-# if len(grid_response['selected_rows']) == 1:
-#     st.subheader("Seleção:")
-#     selecao = fundamentus.get_papel(grid_response['selected_rows'][0]['Papel']) 
-#     st.write(selecao)
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=preco.index, y=preco['Adj Close'], name = 'Preço'))
+fig.add_trace(go.Scatter(x=preco.index, y=preco['MM20'], name= 'MM20'))
+fig.update_layout(title='GRAFICO DE PREÇOS')
+st.plotly_chart(fig)
